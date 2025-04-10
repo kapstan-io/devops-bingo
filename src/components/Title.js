e99373783bcb0f761102f8457782b004cc58c37d
Shyam Kumar <shyam.kumar514@gmail.com>
2025-04-10 13:58:19 -0700

    changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,9 @@
 import TitleSVG from '../assets/svg/Title.svg';
-import YearImage from '../assets/images/2024.png'
 
 export default function Title() {
   return (
     <div className='title-section' style={{ position: 'relative', width: 'fit-content' }}>
       <TitleSVG />
-      <img className='title-2024' src={YearImage.src} alt="2024" />
     </div>
   )
 }
